317034793354a19052845ffe9acbe818a02ece99
Douglas Salinas <dasa72000@gmail.com>
2026-01-27 19:34:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
 import { AppHeader } from "@/components/app-header";
-import dynamic from "next/dynamic";
-import { Skeleton } from "@/components/ui/skeleton";
-
-const FichaMatrimonialForm = dynamic(
-  () => import('@/components/ficha-matrimonial-form').then(mod => mod.FichaMatrimonialForm),
-  { 
-    ssr: false,
-    loading: () => (
-      <div className="space-y-8">
-        <Skeleton className="h-[148px] w-full rounded-lg" />
-        <Skeleton className="h-[438px] w-full rounded-lg" />
-        <Skeleton className="h-[250px] w-full rounded-lg" />
-        <Skeleton className="h-[318px] w-full rounded-lg" />
-        <div className="flex justify-end">
-          <Skeleton className="h-11 w-28 rounded-md" />
-        </div>
-      </div>
-    ),
-  }
-);
-
+import { FichaMatrimonialLoader } from "@/components/ficha-matrimonial-loader";
 
 export default function Home() {
   return (
@@ -27,7 +7,7 @@ export default function Home() {
       <AppHeader />
       <main className="flex-grow flex flex-col items-center p-4 sm:p-8 md:p-12">
         <div className="w-full max-w-5xl">
-          <FichaMatrimonialForm />
+          <FichaMatrimonialLoader />
         </div>
       </main>
     </div>
---
+++ src/components/ficha-matrimonial-loader.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { Skeleton } from "@/components/ui/skeleton";
+
+const FichaMatrimonialForm = dynamic(
+  () => import('@/components/ficha-matrimonial-form').then(mod => mod.FichaMatrimonialForm),
+  { 
+    ssr: false,
+    loading: () => (
+      <div className="space-y-8">
+        <Skeleton className="h-[148px] w-full rounded-lg" />
+        <Skeleton className="h-[438px] w-full rounded-lg" />
+        <Skeleton className="h-[250px] w-full rounded-lg" />
+        <Skeleton className="h-[318px] w-full rounded-lg" />
+        <div className="flex justify-end">
+          <Skeleton className="h-11 w-28 rounded-md" />
+        </div>
+      </div>
+    ),
+  }
+);
+
+export function FichaMatrimonialLoader() {
+  return <FichaMatrimonialForm />;
+}
